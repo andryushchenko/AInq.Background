12fb8472547f16015543009ca2b91860453ea826
Anton Andryushchenko <andryushchenko_anton@outlook.com>
2022-03-20 14:59:35 +0300

    [REF] Code cleanup [UPD] Version

--- src/Shared/QueuedAccessFactory.cs
+++ src/Shared/QueuedAccessFactory.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using AInq.Background.Helpers;
-
 namespace AInq.Background.Tasks;
 
 internal static class QueuedAccessFactory
